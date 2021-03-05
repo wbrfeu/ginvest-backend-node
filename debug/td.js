7a4bbcbe51fe7dcdc29cfef51cd2ed425d37ef40
Marcelo A. Gouveia <magouveia@gmail.com>
2021-03-05 19:51:13 -0300

    scripts para debug

---
+++ debug/td.js
@@ -0,0 +1,19 @@
+import { Database } from './infra/database/database.js'
+import { ProcessaTesouroDireto } from './domain/usecases/tesourodireto.js'
+import { MovimentoTD } from './domain/entities/tesourodireto.js'
+
+var db = new Database()
+let td = new ProcessaTesouroDireto()
+td.setDatabase(db)
+
+// Cria uma nova NN para processar e salvar no banco de dados
+let novaNN = []
+let td10 = new MovimentoTD("M", 10, "Easy", "LFT25", "10/2/2021", "C", null, 100, 1000)
+let td20 = new MovimentoTD("M", 20, "Easy", "LFT25", "12/2/2021", "V", null, 1200, 1210)
+novaNN.push(td10)
+novaNN.push(td20)
+
+let nnProcessada = td.processaNovaNotaNegociacao(novaNN)
+console.log(nnProcessada)
+
+// processaTD.salvaNovaNotaNegociacao(nnProcessada)
