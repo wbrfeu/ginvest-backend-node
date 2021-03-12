7f808862c0b2e80b4fd15a1b4970767d21181aff
Walter <wbrfeu@gmail.com>
2021-03-12 18:27:25 -0300

    implementando bd

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import {inicializaBackend} from './infra/start.js'
+import { Database } from './infra/database/database.js' // TODO - Somente um teste APAGAR
 
 inicializaBackend()
-    .then(() => { console.log(`Sistema ${process.env.APP_NAME} Inicializado`) })
+    .then(async () => {
+        console.log(`Sistema ${process.env.APP_NAME} Inicializado`) 
+        let db = new Database()
+        let recebe = await db.leEstoqueAtualTD()
+        console.log(recebe)
+    })
     .catch((e) => { console.error(`Erro ao Inicializar Sistema - ${e}`) })
 
--- infra/database/database.js
+++ infra/database/database.js
@@ -1,5 +1,42 @@
-import { DatabaseMock } from '../../tests/mocks/database-mock.js'
+import { dbClient } from './start-database.js'
+import { EstoqueTD } from '../../domain/entities/tesourodireto.js'
 
-class Database extends DatabaseMock {}
+class Database {
+
+    async leEstoqueAtualTD() {
+
+        const sql = `SELECT iduser, numeronotanegociacao, idcorretora, codisin, datanegociacao, idlote, 
+                     quantidade, valorliquido FROM movimentotd;`
+        
+        let result = await dbClient.query(sql)
+
+        let estoque = []
+
+        for (let i = 0; i < result.rows.length; i++) {
+            let r = result.rows[i];
+
+            let est = new EstoqueTD(
+                r.iduser, 
+                r.numeronotanegociacao, 
+                r.idcorretora, 
+                r.codisin, 
+                r.datanegociacao, 
+                r.idlote, 
+                r.quantidade, 
+                r.valorliquido
+            )
+
+            estoque.push(est)
+        }
+
+        return estoque
+
+    }
+    
+
+    salvaNovaNotaNegociacao(novaNN) {}
+
+    
+}
 
 export { Database }
