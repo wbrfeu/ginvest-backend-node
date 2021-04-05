85cd787eab86e6f85bacd09f6550a331a124d786
Walter <wbrfeu@gmail.com>
2021-04-05 18:11:14 -0300

    Adição do CORS

--- infra/server/init-server.js
+++ infra/server/init-server.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { logger } from "../logger/logger.js"
 import { inicializaRotas } from "./routes.js"
+import cors from 'cors'
 
 function inicializaServidorHttp() {
     const server = express()
+    server.use(cors())
     server.use(express.urlencoded({ extended: true }))
     server.use(express.json())
 
